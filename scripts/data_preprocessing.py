aa2fe0190f7a8e8945994327054114d751aad4e5
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-07-31 17:29:09 +0530

    Create data_preprocessing.py

---
+++ scripts/data_preprocessing.py
@@ -0,0 +1,14 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+def load_and_preprocess_data():
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32') / 255.0
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32') / 255.0
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+    return X_train, X_test, y_train, y_test
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = load_and_preprocess_data()
